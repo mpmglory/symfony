996bde5b8ab4bf4b43d195f105b05adf9e98e6e7
platinnson <joyplatini@gmail.com>
2017-01-21 23:21:46 +0100

    creation de formulaire externe reussie

--- src/PMM/PlatformBundle/Controller/AdvertController.php
+++ src/PMM/PlatformBundle/Controller/AdvertController.php
@@ -6,13 +6,13 @@ use PMM\PlatformBundle\Entity\Advert;
 use PMM\PlatformBundle\Entity\Image;
 use PMM\PlatformBundle\Entity\Application;
 use PMM\PlatformBundle\Entity\Skill;
-use PMM\PlatformBundle\Entity\AdvertSkill;
 use PMM\PlatformBundle\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use PMM\PlatformBundle\Form\AdvertType;
 
 class AdvertController extends Controller{
 
@@ -75,17 +75,32 @@ class AdvertController extends Controller{
 	}
 	
 	public function addAction(Request $request){
-		
-		$em = $this->getDoctrine()->getManager();
-		
-		if($request->isMethod('POST')){
-			$request->getSession()->getFlashBag()
-			->add('notice', 'Annonce bien enregistree.');
+
+		$advert = new Advert();
+
+		$form = $this->get('form.factory')->create(AdvertType::class, $advert);
 			
-			return $this->redirectToRoute('pmm_platform_view', array('id' => $advert->getId()));
+		if($request->isMethod('POST')){
+			
+			$form->handleRequest($request);
+
+			if ($form->isValid()){
+
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($advert);
+				$em->flush();
+
+				$request->getSession()->getFlashBag()
+					->add('notice', 'Annonce bien enregistree.');
+		
+					return $this->redirectToRoute('pmm_platform_view', array(
+						'id' => $advert->getId()));
+			}
 		}
 	
-		return $this->render('PMMPlatformBundle:Advert:add.html.twig');
+		return $this->render('PMMPlatformBundle:Advert:add.html.twig', array(
+					'form' => $form->createView(),
+				));
 	}
 	
 	public function editAction($id, Request $request){
---
+++ src/PMM/PlatformBundle/Form/AdvertType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace PMM\PlatformBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class AdvertType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('date', DateType::class)
+            ->add('title', TextType::class)
+            ->add('author', TextType::class)
+            ->add('content', TextareaType::class)
+            ->add('published', checkboxType::class)
+            ->add('save', SubmitType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'PMM\PlatformBundle\Entity\Advert'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'pmm_platformbundle_advert';
+    }
+
+
+}
